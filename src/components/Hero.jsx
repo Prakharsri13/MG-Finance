9213fd8577de91bdf9b4ed774aad810b14a7b97e
Prakhar Srivastava <prakharsrivast1410@gmail.com>
2024-11-15 21:29:24 +0530

    small changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -24,7 +24,7 @@ const Hero = () => {
                         </div>
                     </div>
                     <h2 className="headline-1 max-w-[15ch] sm:max-w-[20ch] lg:max-w-[15ch] mt-5 mb-8 lg:mb-10 text-gray-800">
-                        MG Financial Services एक अटूट रिश्ता विश्वास का
+                        MG Financial Services An Unbreakable Bond of Trust
                     </h2>
                     <div className="flex items-center gap-3">
                         <ButtonPrimary
